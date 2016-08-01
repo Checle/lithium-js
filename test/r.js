230e3822b5720933a4ce16baacf05bc69cfc961c
Filip Dalüge <daluege@gmail.com>
2016-08-01 18:39:04 +0200

    Test layers and linkage #12

--- test/r.js
+++ test/r.js
@@ -13,11 +13,12 @@ test('record', (t) => {
   t.equal(record('block')(), content);
 
   var dir = record('/test/', () => this); // Function that remains in its scope
-  dir('file1');
-  dir('file2');
+  dir('file0');
+  dir('file1', () => true); // Branch updates owner.target but not owner.next
+  dir('file2')(() => true);
   dir('file3');
   for (var dirs = [], next = dir; next = next(); dirs.push(next.toString()));
-  t.deepLooseEqual(dirs, ['file1', 'file2', 'file3']);
+  t.deepLooseEqual(dirs, ['file0', 'file1', 'file2', 'file3']);
 
   var acceptor = () => 3; // Accepts anything and returns 3
   dir('file4', acceptor);
@@ -27,6 +28,20 @@ test('record', (t) => {
   // Next value of file4 should be the unaltered raw acceptor
   t.equal(file4(), acceptor);
   t.equal(file4()(), 3);
+
+  var dir = record('/test/');
+  record('/test/file0/', '..', dir); // Directory link
+  t.equal(record('/test/file2/..')()(), true);
+
+  // FIXME
+  // Add a new block: branches target
+  block(() => this); // Blocks constitute a sequence of equal levels
+  block(1); // Create an initial block
+  block(2);
+  record('advanced', 1); // Record top-level entity
+  t.equal(record('advanced').valueOf(), 1);
+  t.equal(block(2)('advanced').valueOf(), 1);
+  t.deepLooseEqual(block(1)('advanced').valueOf(), null);
 
   t.end();
 });
