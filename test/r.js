059ce4b551612b0db81ec69112ed00cfea66fb33
Filip Dalüge <daluege@gmail.com>
2016-08-01 18:06:14 +0200

    Test acceptors and sequences #12

--- test/r.js
+++ test/r.js
@@ -6,20 +6,27 @@ test('record', (t) => {
   record(() => undefined); // Function that accepts anything
 
   var block = record('block'); // Key
-
   t.equal(block.toString(), 'block');
 
   var content = {}, func = block(() => content);
-
   t.equal(func(), content);
   t.equal(record('block')(), content);
 
-  var dir = record('/test/', () => this);
+  var dir = record('/test/', () => this); // Function that remains in its scope
   dir('file1');
   dir('file2');
   dir('file3');
   for (var dirs = [], next = dir; next = next(); dirs.push(next.toString()));
   t.deepLooseEqual(dirs, ['file1', 'file2', 'file3']);
+
+  var acceptor = () => 3; // Accepts anything and returns 3
+  dir('file4', acceptor);
+  t.equal(dir('file4', 'attr'), 3); // Any attribute of file4 should equal 3
+
+  var file4 = dir('file4');
+  // Next value of file4 should be the unaltered raw acceptor
+  t.equal(file4(), acceptor);
+  t.equal(file4()(), 3);
 
   t.end();
 });
