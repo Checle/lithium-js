f26f2cfde01fcdd11a4493a7096287dee3e10e59
Filip Dalüge <daluege@gmail.com>
2016-08-22 05:42:48 +0200

    Updates

--- type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var cs = require('./type/cs.ts');
-exports.cs = cs;
-var segment = require('./type/segment.ts');
-exports.segment = segment;
-var vn = require('./type/vn.ts');
-exports.vn = vn;
--- type/varint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function read(stream) {
-    return stream.shiftUInt32LE();
-}
-exports.read = read;
-function write(stream, number) {
-    stream.pushUInt32LE(number);
-}
-exports.write = write;
