c1c8885e30db711664a81fefb278367a2e665204
Filip Dalüge <daluege@gmail.com>
2016-09-03 01:24:39 +0200

    Incrementally copy buffer lists featured by fork

--- src/util/buffer-list.ts
+++ src/util/buffer-list.ts
@@ -1,41 +1,52 @@
-/**
- * @source https://github.com/nodejs/node/blob/master/lib/internal/streams/BufferList.js
- */
+import { fork } from 'object-fork'
 
 class Entry {
   data: Buffer
   next: Entry
+  prev: Entry
 }
 
 export default class BufferList {
-  constructor () {
-    this.head = null
-    this.tail = null
-    this.length = 0
+  head: Entry = null
+  tail: Entry = null
+  length: number = 0
+
+  toString () {
+    return this.join('')
   }
 
-  head: Entry
-  tail: Entry
-  length: number
-
   push (v) {
-    const entry: Entry = { data: v, next: null }
+    const entry: Entry = { data: v, next: null, prev: this.tail }
     if (this.length > 0) {
       this.tail.next = entry
     } else {
       this.head = entry
     }
-    this.tail = entry;
+    this.tail = entry
     ++this.length
   }
 
   unshift (v) {
-    const entry = { data: v, next: this.head }
+    const entry = { data: v, next: this.head, prev: null }
     if (this.length === 0) {
       this.tail = entry
     }
-    this.head = entry;
+    this.head = entry
     ++this.length
+  }
+
+  pop () {
+    if (this.length === 0) {
+      return
+    }
+    const ret = this.tail.data
+    if (this.length === 1) {
+      this.head = this.tail = null
+    } else {
+      this.tail = this.tail.prev
+    }
+    --this.length
+    return ret
   }
 
   shift () {
@@ -87,12 +98,42 @@ export default class BufferList {
     return ret
   }
 
+  splice (index: number, count?: number, data?: Buffer): void {
+    var p = this.head
+    var i
+    for (i = 0; i < index && p; i++) p = p.next
+    if (!p) {
+      if (data) return this.push(data)
+      return
+    }
+    p = p.prev
+    if (count == null) {
+      p.next = null
+    } else for (i = 0; i < count && p.next; i++) {
+      this.length -= p.next.data.length
+      p.next.prev = p
+      p.next = p.next.next
+    }
+    if (!p.next) this.tail = p
+    if (data) {
+      if (!p.next) return this.push(data)
+      p.next = p.next.prev = { data: data, prev: p, next: p.next }
+    }
+  }
+
+  slice (begin?: number, end?: number) {
+    var copy = fork(this)
+    copy.splice(0, begin)
+    copy.splice(begin)
+    return copy
+  }
+
   compare (target) {
     if (!Buffer.isBuffer(target)) {
       target = new Buffer(String(target))
     }
     var p = this.head
-    var i = 0;
+    var i = 0
     while (p && i <= target.length) {
       var cmp = p.data.compare(target)
       if (cmp) return cmp
@@ -102,10 +143,6 @@ export default class BufferList {
     if (target.length == this.length) {
       return 0
     }
-    return target.length > this.length ? 1 : -1
-  }
-
-  toString () {
-    return this.join('')
+    return target.length < this.length ? 1 : -1
   }
 }
