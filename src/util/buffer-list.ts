2b2a0c68a45e7ab5900fd668a79eadb077e9c08b
Filip Dalüge <daluege@gmail.com>
2016-09-02 17:06:03 +0200

    Convert BufferList to ES6

--- src/util/buffer-list.ts
+++ src/util/buffer-list.ts
@@ -2,77 +2,88 @@
  * @source https://github.com/nodejs/node/blob/master/lib/internal/streams/BufferList.js
  */
 
-function BufferList () {
-  this.head = null
-  this.tail = null
-  this.length = 0
+class Entry {
+  data: Buffer
+  next: Entry
 }
 
-BufferList.prototype.push = function (v) {
-  const entry = { data: v, next: null }
-  if (this.length > 0) {
-    this.tail.next = entry
-  } else {
-    this.head = entry
+export default class BufferList {
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.length = 0
   }
-  this.tail = entry;
-  ++this.length
-}
 
-BufferList.prototype.unshift = function (v) {
-  const entry = { data: v, next: this.head }
-  if (this.length === 0) {
-    this.tail = entry
-  }
-  this.head = entry;
-  ++this.length
-}
+  head: Entry
+  tail: Entry
+  length: number
 
-BufferList.prototype.shift = function () {
-  if (this.length === 0) {
-    return
+  push (v) {
+    const entry: Entry = { data: v, next: null }
+    if (this.length > 0) {
+      this.tail.next = entry
+    } else {
+      this.head = entry
+    }
+    this.tail = entry;
+    ++this.length
   }
-  const ret = this.head.data
-  if (this.length === 1) {
+
+  unshift (v) {
+    const entry = { data: v, next: this.head }
+    if (this.length === 0) {
+      this.tail = entry
+    }
+    this.head = entry;
+    ++this.length
+  }
+
+  shift () {
+    if (this.length === 0) {
+      return
+    }
+    const ret = this.head.data
+    if (this.length === 1) {
+      this.head = this.tail = null
+    } else {
+      this.head = this.head.next
+    }
+    --this.length
+    return ret
+  }
+
+  clear () {
     this.head = this.tail = null
-  } else {
-    this.head = this.head.next
+    this.length = 0
   }
-  --this.length
-  return ret
-}
 
-BufferList.prototype.clear = function () {
-  this.head = this.tail = null
-  this.length = 0
-}
+  join (s) {
+    if (this.length === 0) {
+      return ''
+    }
+    var p = this.head
+    var ret = '' + p.data
+    while ((p = p.next)) {
+      ret += s + p.data
+    }
+    return ret
+  }
 
-BufferList.prototype.join = function (s) {
-  if (this.length === 0) {
-    return ''
+  concat (n) {
+    if (this.length === 0) {
+      return new Buffer(0)
+    }
+    if (this.length === 1) {
+      return this.head.data
+    }
+    const ret = new Buffer(n >>> 0)
+    var p = this.head
+    var i = 0
+    while (p) {
+      p.data.copy(ret, i)
+      i += p.data.length
+      p = p.next
+    }
+    return ret
   }
-  var p = this.head
-  var ret = '' + p.data
-  while ((p = p.next)) {
-    ret += s + p.data
-  }
-  return ret
-}
-
-BufferList.prototype.concat = function (n) {
-  if (this.length === 0) {
-    return new Buffer(0)
-  }
-  if (this.length === 1) {
-    return this.head.data
-  }
-  const ret = new Buffer(n >>> 0)
-  var p = this.head
-  var i = 0
-  while (p) {
-    p.data.copy(ret, i)
-    i += p.data.length
-    p = p.next
-  }
-  return ret
 }
