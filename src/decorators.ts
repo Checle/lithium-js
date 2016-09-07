5268363dcf7c39fa9047492289b1556cd5e5ad79
Filip Dalüge <daluege@gmail.com>
2016-09-07 16:30:04 +0200

    Add alias `mixin`

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,3 +1,10 @@
+export { forkable } from './util/fork'
+export { mergeable } from './util/merge'
+
+/**
+ * Decorator that assigns properties of `value` or its prototype to
+ * the prototype of the target.
+ */
 export function prototype (value) {
   if (typeof value === 'function') value = value.length ? Object.create(value.prototype) : new value()
 
@@ -6,3 +13,8 @@ export function prototype (value) {
     else Object.assign(target.prototype, value) // Class decorator
   }
 }
+
+/**
+ * Alias for `prototype`.
+ */
+export const mixin = prototype
