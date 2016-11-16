24ea6fe48368a1fc48802eb7262d6dc24c4c71fd
Filip Dalüge <daluege@gmail.com>
2016-11-16 05:17:28 +0100

    Global boot

--- src/boot.ts
+++ src/boot.ts
@@ -1,18 +1,3 @@
 #!/usr/bin/node
 
-import './sys/global'
-
-import * as local from './sys/lib/io/local'
-import {posix as path} from 'path'
-import {uselib} from 'unistd'
-import {mount} from 'sys/mount'
-
-environ = {
-  PATH: ['bin', 'usr/bin'].map(pathname => path.resolve(pathname)).join(':'),
-  JSPATH: ['sys/module', 'lib/module', 'usr/lib/module', 'usr/lib/node_modules', 'usr/include'].map(pathname => path.resolve(pathname)).join(':'),
-}
-
-uselib('lib/io/local')
-
-mount(local, '/usr/local')
-mount(__dirname, '/')
+import 'boot'
